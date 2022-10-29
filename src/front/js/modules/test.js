bf6def95f1e25cfb97919dda1ab85f26190031e0
sergiodsg <sergiodasilva.dev@gmail.com>
2022-10-29 01:33:29 +0000

    Añadidos tests a la función que ejecuta la lógica de la comparación para la búsqueda por nombres

---
+++ src/front/js/modules/test.js
@@ -0,0 +1,22 @@
+const {nameMatch} = require("../modules/nameMatch");
+
+test('Not to be null', () => {
+    expect(nameMatch("first string","second string")).not.toBeNull();
+    expect(nameMatch("same string","same string")).not.toBeNull();
+})
+
+test('Not to be undefined', () => {
+    expect(nameMatch("first string","second string")).not.toBeUndefined();
+    expect(nameMatch("same string","same string")).not.toBeUndefined();
+})
+
+test('True', () => {
+    expect(nameMatch("same string","same string")).toBeTruthy();
+})
+
+test('False', () => {
+    expect(nameMatch("first string","second string")).toBeFalsy();
+})
+
+
+
